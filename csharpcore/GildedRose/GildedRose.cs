0781b8cc7713a45c208d01eaf08029c2fd89d585
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2023-12-07 20:24:05 +0000

    Move each block into its own method

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -20,14 +20,79 @@ public class GildedRose
         {
             var config = _itemConfigFactory.Get(item);
 
-            if (item.Name != Constants.AgedBrie && item.Name != Constants.BackstagePasses)
+            ApplyPreSellinQualityOffset(item);
+            ApplySellinOffset(item);
+            ApplyPostSellinQualityOffset(item);
+        }
+    }
+
+    private void ApplyPreSellinQualityOffset(Item item)
+    {
+        if (item.Name != Constants.AgedBrie && item.Name != Constants.BackstagePasses)
+        {
+            if (item.Quality > 0)
             {
-                if (item.Quality > 0)
+                if (item.Name != Constants.Sulfuras)
                 {
-                    if (item.Name != Constants.Sulfuras)
+                    item.Quality = item.Quality - 1;
+                }
+            }
+        }
+        else
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.Name == Constants.BackstagePasses)
+                {
+                    if (item.SellIn < 11)
                     {
-                        item.Quality = item.Quality - 1;
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
                     }
+
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void ApplySellinOffset(Item item)
+    {
+        if (item.Name != Constants.Sulfuras)
+        {
+            item.SellIn = item.SellIn - 1;
+        }
+    }
+        
+    private void ApplyPostSellinQualityOffset(Item item)
+    {
+        if (item.SellIn < 0)
+        {
+            if (item.Name != Constants.AgedBrie)
+            {
+                if (item.Name != Constants.BackstagePasses)
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != Constants.Sulfuras)
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality = item.Quality - item.Quality;
                 }
             }
             else
@@ -35,58 +100,6 @@ public class GildedRose
                 if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
-
-                    if (item.Name == Constants.BackstagePasses)
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != Constants.Sulfuras)
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != Constants.AgedBrie)
-                {
-                    if (item.Name != Constants.BackstagePasses)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != Constants.Sulfuras)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
                 }
             }
         }
