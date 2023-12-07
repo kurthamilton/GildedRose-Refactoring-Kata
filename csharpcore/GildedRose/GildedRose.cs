68aada16dec01076a3fdabe9b2aedb6d4894e8ec
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2023-12-07 19:46:03 +0000

    Use foreach for slight readability improvement

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -13,74 +13,74 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < _items.Count; i++)
+        foreach (Item item in _items)
         {
-            if (_items[i].Name != Constants.AgedBrie && _items[i].Name != Constants.BackstagePasses)
+            if (item.Name != Constants.AgedBrie && item.Name != Constants.BackstagePasses)
             {
-                if (_items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    if (_items[i].Name != Constants.Sulfuras)
+                    if (item.Name != Constants.Sulfuras)
                     {
-                        _items[i].Quality = _items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }
                 }
             }
             else
             {
-                if (_items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    _items[i].Quality = _items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (_items[i].Name == Constants.BackstagePasses)
+                    if (item.Name == Constants.BackstagePasses)
                     {
-                        if (_items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (_items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
 
-                        if (_items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (_items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (_items[i].Name != Constants.Sulfuras)
+            if (item.Name != Constants.Sulfuras)
             {
-                _items[i].SellIn = _items[i].SellIn - 1;
+                item.SellIn = item.SellIn - 1;
             }
 
-            if (_items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (_items[i].Name != Constants.AgedBrie)
+                if (item.Name != Constants.AgedBrie)
                 {
-                    if (_items[i].Name != Constants.BackstagePasses)
+                    if (item.Name != Constants.BackstagePasses)
                     {
-                        if (_items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (_items[i].Name != Constants.Sulfuras)
+                            if (item.Name != Constants.Sulfuras)
                             {
-                                _items[i].Quality = _items[i].Quality - 1;
+                                item.Quality = item.Quality - 1;
                             }
                         }
                     }
                     else
                     {
-                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
                 else
                 {
-                    if (_items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        _items[i].Quality = _items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
