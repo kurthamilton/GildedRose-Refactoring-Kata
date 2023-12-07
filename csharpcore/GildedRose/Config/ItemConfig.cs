9503abce3e799335ae7c6cda66900f3358bb5e95
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2023-12-07 21:38:07 +0000

    Introduce the concept of thresholds

--- csharpcore/GildedRose/Config/ItemConfig.cs
+++ csharpcore/GildedRose/Config/ItemConfig.cs
@@ -1,13 +1,19 @@
-﻿namespace GildedRoseKata.Config;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GildedRoseKata.Config;
 
 public class ItemConfig
 {
-    public ItemConfig(int max, int min, int defaultOffset, int sellInOffset)
+    public ItemConfig(int max, int min, int defaultOffset, int sellInOffset, 
+        IEnumerable<ItemConfigQualityOffsetThresholds> thresholds)
     {
         DefaultOffset = defaultOffset;
         Max = max;
         Min = min;
         SellInOffset = sellInOffset;
+        Thresholds = thresholds?.ToArray() ?? Array.Empty<ItemConfigQualityOffsetThresholds>();
     }
 
     public int DefaultOffset { get; }
@@ -17,4 +23,6 @@ public class ItemConfig
     public int Min { get; }
 
     public int SellInOffset { get; }
+
+    public IReadOnlyCollection<ItemConfigQualityOffsetThresholds> Thresholds { get; }
 }
--- csharpcore/GildedRose/Config/ItemConfigFactory.cs
+++ csharpcore/GildedRose/Config/ItemConfigFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace GildedRoseKata.Config;
+﻿using System;
+
+namespace GildedRoseKata.Config;
 
 public class ItemConfigFactory : IItemConfigFactory
 {
@@ -6,12 +8,21 @@ public class ItemConfigFactory : IItemConfigFactory
     {
         return item.Name switch
         {
-            Constants.AgedBrie => new ItemConfig(max: 50, min: 0, defaultOffset: 1, sellInOffset: -1),
-            Constants.BackstagePasses => new ItemConfig(max: 50, min: 0, defaultOffset: 1, sellInOffset: -1),
+            Constants.AgedBrie =>new ItemConfig(max: 50, min: 0, defaultOffset: 1, sellInOffset: -1, 
+                thresholds: new[] 
+                { 
+                    new ItemConfigQualityOffsetThresholds(null, 0, 2) 
+                }),
+            Constants.BackstagePasses => new ItemConfig(max: 50, min: 0, defaultOffset: 1, sellInOffset: -1, 
+                thresholds: new[] 
+                { 
+                    new ItemConfigQualityOffsetThresholds(1, 5, 3),
+                    new ItemConfigQualityOffsetThresholds(6, 10, 2)
+                }),
             // ignore conjured for now
             // Constants.Conjured =>        new ItemConfig(max: 50, min: 0,  defaultOffset: -2),
-            Constants.Sulfuras => new ItemConfig(max: 80, min: 80, defaultOffset: 0, sellInOffset: 0),
-            _ => new ItemConfig(max: 50, min: 0, defaultOffset: -1, sellInOffset: -1)
+            Constants.Sulfuras => new ItemConfig(max: 80, min: 80, defaultOffset: 0,  sellInOffset: 0, thresholds: null),
+            _ => new ItemConfig(max: 50, min: 0, defaultOffset: -1, sellInOffset: -1, thresholds: null)
         };
     }
 }
